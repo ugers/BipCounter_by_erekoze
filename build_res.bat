78e5cb2470a0fae5efc70f025de8c38788829221
ugers <yugers@gmail.com>
2020-09-01 01:09:26 +0300

    fix repack

--- build_res.bat
+++ build_res.bat
@@ -1,2 +1,2 @@
 @echo off
-ResPack.exe -a MNVolkov_BipOS_0.5.3_Apps_Mili_chaohu.res.latin ./stock/MNVolkov_BipOS_0.5.3_Apps_Mili_chaohu.res.latin BipCounter.elf
+ResPack.exe -a MNVolkov_BipOS_0.5.3_Apps_Mili_chaohu.res.latin ./stock/MNVolkov_BipOS_0.5.3_Apps_Mili_chaohu.res.latin BipCounter_by_erekoze.elf
